19ac3dba708d182580d0e96636ff2b67087747df
SaeedZhiany <SaeedZhiany@users.noreply.github.com>
2026-05-18 10:37:36 +0330

    implemented new TWA example according to v2.4.0 adtrace web sdk

--- app/src/main/java/com/example/twaexample/LaunchTwaActivity.java
+++ app/src/main/java/com/example/twaexample/LaunchTwaActivity.java
@@ -1,54 +1,36 @@
 package com.example.twaexample;
 
-
 import android.net.Uri;
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
-
-import com.google.androidbrowserhelper.trusted.LauncherActivity;
-
 import io.adtrace.sdk.AdTrace;
-import io.adtrace.sdk.OnDeviceIdsRead;
 
-public class LaunchTwaActivity extends LauncherActivity {
-    private String gpsAdId;
+public class LaunchTwaActivity extends com.google.androidbrowserhelper.trusted.LauncherActivity {
+
+    private String mGoogleAdId;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // `super.onCreate()` may have called `finish()`. In this case, we don't do any work.
-        if (isFinishing()) {
-            return;
-        }
-
-        AdTrace.getGoogleAdId(this, new OnDeviceIdsRead() {
-            @Override
-            public void onGoogleAdIdRead(String googleAdId) {
-                gpsAdId = googleAdId;
-                launchTwa();
-
-            }
+        AdTrace.getGoogleAdId(this, googleAdId -> {
+            mGoogleAdId = googleAdId != null ? googleAdId : "";
+            launchTwa();
         });
-
-
     }
 
     @Override
     protected boolean shouldLaunchImmediately() {
-        // launchImmediately() returns `false` so we can wait until Firebase Analytics is ready
-        // and then launch the Trusted Web Activity with `launch()`.
         return false;
     }
 
     @Override
     protected Uri getLaunchingUrl() {
-        Uri uri = super.getLaunchingUrl();
-        // Attach the Firebase instance Id to the launchUrl. This example uses "appInstanceId" as
-        // the parameter name.
-        return uri.buildUpon()
-                .appendQueryParameter("gps_ad_id", gpsAdId)
+        Uri base = Uri.parse("YOUR_WEB_PAGE_BASE_URL");
+        if (mGoogleAdId == null) {
+            mGoogleAdId = "";
+        }
+        return base.buildUpon()
+                .appendQueryParameter("gps_adid", mGoogleAdId)
                 .build();
     }
 }
